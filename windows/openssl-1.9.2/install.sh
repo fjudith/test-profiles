735e665d1df8d4216b37d4ed9be1af758330ca40
Florian JUDITH <florian.judith.b@gmail.com>
2019-03-04 21:07:34 +0100

    Changed download URL for Linux

--- windows/openssl-1.9.2/install.sh
+++ windows/openssl-1.9.2/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-tar -zxvf openssl-1.0.2q.tar.gz
+tar -zxvf OpenSSL_1_0_2q.tar.gz
 
-cd openssl-1.0.2q/
+cd openssl-OpenSSL_1_0_2q/
 ./config no-zlib
 make
 echo \$? > ~/install-exit-status
 cd ~
 
 echo "#!/bin/sh
-./openssl-1.0.2q/apps/openssl speed rsa4096 > \$LOG_FILE 2>&1
+./openssl-OpenSSL_1_0_2q/apps/openssl speed rsa4096 > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > openssl
 chmod +x openssl
 
